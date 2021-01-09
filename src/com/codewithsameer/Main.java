93604ec2e28b4cad6cc2a3a3ae75ca889bac4c00
sameeruj <sameeruj@yahoo.com>
2021-01-09 07:00:27 +0530

    First Java commit.

---
+++ src/com/codewithsameer/Main.java
@@ -0,0 +1,15 @@
+package com.codewithsameer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("hello ");
+        int temp = 20;
+        int age = 35;
+        String name = "Sameer";
+        System.out.print("My name is ");
+        System.out.print(name);
+        System.out.print(" and my age is ");
+        System.out.println(age);
+    }
+}
