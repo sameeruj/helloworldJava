3fab7fe2a917d899e53961f07993ea2bd9f4a155
sameeruj <sameeruj@yahoo.com>
2021-01-09 07:20:59 +0530

    Second Java commit.

--- src/com/codewithsameer/Main.java
+++ src/com/codewithsameer/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("hello ");
         int temp = 20;
-        int age = 33;
+        int age = 39;
         String name = "Sameer";
         System.out.print("My name is ");
         System.out.print(name);
